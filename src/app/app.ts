d627a1bd52a23666659a3702cd02da599a965a3f
jasim00021 <sk.jasimuddin@techwens.in>
2023-06-09 15:25:55 +0530

    node version add in package.json

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,11 +8,24 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 
+class ApiError extends Error {
+  statusCode: number
+  constructor(statusCode: number, message: string | undefined, stack = '') {
+    super(message)
+    this.statusCode = statusCode
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
 app.get('/', async (req: Request, res: Response) => {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  throw new ApiError(400, 'Internal Server Error')
 })
 // user Router
 app.use('/api/v1/user', userRouter)
-app.use('/api/v1/')
 
 export default app
